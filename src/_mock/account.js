808172e00a4484ab17a724364a3fca4862b9c289
DevinBurhan <burhanuddin.devstree@gmail.com>
2024-07-22 11:45:44 +0530

    first commit

---
+++ src/_mock/account.js
@@ -0,0 +1,7 @@
+// ----------------------------------------------------------------------
+
+export const account = {
+  displayName: 'Burhan Rampura',
+  email: 'burhanuddin.devstree@gmail.com',
+  photoURL: '/assets/images/avatars/avatar_25.jpg',
+};
